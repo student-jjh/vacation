d6d3cafaf4d6924aad9b39dc74d9024d10b7152e
JJH <skkuwjswnsgur456@gmail.com>
2021-07-27 01:24:22 +0900

    FBV

---
+++ django/blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #여기를 채울 예정
+    path('',views.index),
+]
--- django/blog/views.py
+++ django/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def index(request):
+    posts=Post.objects.all().order_by('-pk') #최신 글부터 올라오도록 하기 위해 작업
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts' : posts,
+        }
+    )
